5e30966d668c852b6f9d788d62961967668191ef
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-12-22 19:07:06 +0100

    milestone 1

--- js/script.js
+++ js/script.js
@@ -2,21 +2,27 @@ var boolflix = new Vue ({
   el : '#root',
   data : {
     films:[],
-    searchInput: ' '
+    searchInput: ' ',
+    points: ''
   },
-  methods:  function() {
-    var self = this
-    axios
-    .get('https://api.themoviedb.org/3/search/movie', {
-      params: {
-        api_key: '5cb0b53521cc9c3b16476bcbe94fe787',
-        query: self.searchInput,
-        language: 'it-IT'
-      }
-    })
-    .then(function (response) {
-      self.films = response.data.results;
-      console.log(self.films);
-    })
+  methods:{
+    searchMovies: function() {
+      var self = this
+      axios
+      .get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          api_key: '5cb0b53521cc9c3b16476bcbe94fe787',
+          query: self.searchInput,
+          language: 'it-IT'
+        }
+      })
+      .then(function (response) {
+        self.films = response.data.results;
+        var completeRate=parseInt(self.films.vote_average/2);
+        var rateRound = parseInt(Math.round(completeRate);
+        self.points = rateRound
+      })
+    }
+
   }
 });
