b70ab93c0528925871e448fb687bedad01d163e0
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-12-22 15:41:33 +0100

    Quarto commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 var boolflix = new Vue ({
   el : '#root',
   data : {
-
+    films:[],
+    searchInput: ' '
+  },
+  methods:  function() {
+    var self = this
+    axios
+    .get('https://api.themoviedb.org/3/search/movie', {
+      params: {
+        api_key: '5cb0b53521cc9c3b16476bcbe94fe787',
+        query: self.searchInput,
+        language: 'it-IT'
+      }
+    })
+    .then(function (response) {
+      self.films = response.data.results;
+      console.log(self.films);
+    })
   }
 });
